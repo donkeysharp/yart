a6733db748d5b7fa6d79721a5d17cc64ea98cbc5
Sergio Guillen <serguimant@gmail.com>
2016-07-18 14:16:43 -0400

    Fixed routes definition

--- src/javascript/routes.js
+++ src/javascript/routes.js
@@ -4,7 +4,7 @@ import AppContainer from './containers/AppContainer';
 import Module1Container from './containers/Module1Container';
 import Module2Container from './containers/Module2Container';
 
-let routes = (
+const routes = (
   <Route path="/" component={AppContainer}>
     <Route path="sub1/:id" component={Module1Container} />
     <Route path="sub2/:id" component={Module2Container} />
