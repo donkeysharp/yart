0aabeb2ff22a36d998003ebeacaed584104e3dd3
Sergio Guillen <serguimant@gmail.com>
2016-10-11 23:27:44 -0400

    Redefined initial structure to be simpler

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import {render} from 'react-dom';
-import RootContainer from './containers/RootContainer';
+import {Provider} from 'react-redux';
+import {Router, browserHistory} from 'react-router';
+import {syncHistoryWithStore} from 'react-router-redux';
 import {configureStore} from './store';
+import routes from './routes'
 
 
 document.addEventListener('DOMContentLoaded', function() {
   let store = configureStore();
+  let history = syncHistoryWithStore(browserHistory, store);
+
   render(
-    <RootContainer store={store} />,
+    <Provider store={store}>
+      <Router history={history} routes={routes} />
+    </Provider>,
     document.getElementById('root')
   );
 });
--- src/javascript/store.js
+++ src/javascript/store.js
@@ -1,7 +1,6 @@
 import {applyMiddleware, createStore} from 'redux';
 import thunk from 'redux-thunk';
-// import {routerMiddleware} from 'react-router-redux';
-import rootReducer from '../reducers';
+import rootReducer from './reducers';
 
 
 export function configureStore(initialState={}) {
